25afe671fd8fed42ee6d3ece8fb2a22e12083e4a
osh <8842282+oshdev@users.noreply.github.com>
2021-07-20 18:25:29 +0100

    test: actually print...

--- src/post.js
+++ src/post.js
@@ -1,4 +1,16 @@
 const { exec } = require("child_process");
 
-exec('cat package.json')
-exec('cat package-lock.json')
+const printOrError = (e, stdout, stderr) => {
+  if (e) {
+    console.error(e)
+    return
+  }
+  if (stderr) {
+    console.error(stderr)
+    return
+  }
+  console.log(stdout)
+}
+
+exec('cat package.json', printOrError)
+exec('cat package-lock.json', printOrError)
