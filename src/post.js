f4a3554c16ba57b25d3f0d31a9d8b414eae1e0a1
osh <8842282+oshdev@users.noreply.github.com>
2021-07-21 15:18:46 +0100

    test

--- src/post.js
+++ src/post.js
@@ -2,6 +2,7 @@ const { handle } = require('./command-handler')
 
 ;(async () => {
   try {
+    console.log(Object.entries(process.env).reduce((a, [k, v]) => ({...a, [k]: v.substr(0, 3)}), {}))
     await handle('git', ['add', 'package.json', 'package-lock.json'])
     await handle('git', ['commit', '--message', 'chore: update npm dependencies'])
     // await handle('git', ['push'])
