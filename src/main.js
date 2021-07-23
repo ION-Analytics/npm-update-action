606f90c7a023eeb0bb74c48321c8d04d0f7432de
osh <8842282+oshdev@users.noreply.github.com>
2021-07-23 11:37:44 +0100

    feat: update package.json and package-lock.json 🎉

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ const { handle } = require('./lib')
 
 ;(async () => {
   try {
+    // this is fucking bonkers
+    // install needs to create node_modules so the update command
+    // will update both package.json and package-lock.json
     await handle('npm', ['install', '--ignore-scripts'])
+    await handle('npm', ['update'])
   } catch (e) {
     let code = 1
     if (typeof e === 'number') {
